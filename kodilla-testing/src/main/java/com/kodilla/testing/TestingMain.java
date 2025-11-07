bf9454d8e97a0b15f8ce6182b13fbf607b2cb565
Artur Flisek <artur.flisek@pentacomp.pl>
2025-11-07 15:55:45 +0100

    6.1 Added extra exercise

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import java.util.ArrayList;
+
 public class TestingMain  {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        ArrayList<Character> vowels = findVowels("Adam");
+
+        vowels.forEach(System.out::print);
+    }
+
+    public static ArrayList<Character> findVowels(String string){
+        String vowels = "aeiouAEIOU";
+        ArrayList<Character> vowelsList = new ArrayList<>();
+        for(char c : vowels.toCharArray()){
+            vowelsList.add(c);
+        }
+
+        ArrayList<Character> findVowels = new ArrayList<>();
+
+        for(Character c : string.toCharArray()){
+            if(vowelsList.contains(c)){
+                findVowels.add(c);
+            }
+        }
+
+        return findVowels;
     }
 }
