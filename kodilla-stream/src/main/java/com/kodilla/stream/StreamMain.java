20750220088f25b333805b637de21975ac0245a7
Artur Flisek <artur.flisek@pentacomp.pl>
2025-11-14 18:53:01 +0100

    7.2 Dodanie materiałów i zadania

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
@@ -57,30 +58,9 @@ public class StreamMain  {
 
             return result.toString();
         }));
-        poemBeautifier.beautify("ala ma kota, a kto ma ale", StreamMain::toCamelCase);
+        poemBeautifier.beautify("ala ma kota, a kto ma ale", PoemBeautifier::toCamelCase);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
-
-    public static String toCamelCase(String text) {
-        if (text == null || text.isEmpty()) {
-            return text;
-        }
-
-        String[] words = text.toLowerCase().trim().split("[^a-zA-Z0-9]+");
-
-        if (words.length == 1) {
-            return words[0];
-        }
-
-        StringBuilder sb = new StringBuilder(words[0]); // pierwsze słowo: mała litera
-
-        for (int i = 1; i < words.length; i++) {
-            if (words[i].length() > 0) {
-                sb.append(Character.toUpperCase(words[i].charAt(0)))
-                  .append(words[i].substring(1));
-            }
-        }
-
-        return sb.toString();
-    }
-
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -5,4 +5,27 @@ public class PoemBeautifier {
     public void beautify(String poem, PoemDecorator poemDecorator) {
         System.out.println("Beautify poem: " + poemDecorator.decorate(poem));
     }
+
+    public static String toCamelCase(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+
+        String[] words = text.toLowerCase().trim().split("[^a-zA-Z0-9]+");
+
+        if (words.length == 1) {
+            return words[0];
+        }
+
+        StringBuilder sb = new StringBuilder(words[0]); // pierwsze słowo: mała litera
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() > 0) {
+                sb.append(Character.toUpperCase(words[i].charAt(0)))
+                  .append(words[i].substring(1));
+            }
+        }
+
+        return sb.toString();
+    }
 }
