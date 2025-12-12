b934fb171918ea745c0c9fab1dddd1574fb2e542
Artur Flisek <artur.flisek@pentacomp.pl>
2025-12-12 19:42:46 +0100

    11.6 Dodanie materiałów i zadania

--- kodilla-patterns/src/main/java/com/kodilla/patterns/KodillaPatternsApplication.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/KodillaPatternsApplication.java
@@ -3,11 +3,39 @@ package com.kodilla.patterns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigInteger;
+
 @SpringBootApplication
 public class KodillaPatternsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaPatternsApplication.class, args);
+
+        try {
+            System.out.println(KodillaPatternsApplication.fibonacci(155));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 
+
+    public static BigInteger fibonacci(int index, BigInteger... numbers) throws Exception {
+        if (index < 0) throw new Exception("Wartość powinna być większa od zera");
+        BigInteger current, next;
+
+        if(numbers == null || numbers.length == 0){
+            current = BigInteger.ZERO;
+            next = BigInteger.ONE;
+        } else {
+            current = numbers[0];
+            next = numbers[1];
+        }
+
+        if(index == 0){
+            return current;
+        }
+        else {
+            return fibonacci(index - 1, next, next.add(current));
+        }
+    }
 }
