7471557a6a20493860ab95763d5630ca60f41473
Artur Flisek <artur.flisek@pentacomp.pl>
2025-12-13 13:59:24 +0100

    12.4 Dodanie pliku z zapytaniami i zadania dodatkowego

--- kodilla-patterns/src/main/java/com/kodilla/patterns/KodillaPatternsApplication.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/KodillaPatternsApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 @SpringBootApplication
 public class KodillaPatternsApplication {
@@ -16,6 +18,38 @@ public class KodillaPatternsApplication {
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
+
+        System.out.println("Dla wartości 3,1,5,6,7,9,15,3,4,7,10");
+        KodillaPatternsApplication.findMinAndMaxSimple(3,1,5,6,7,9,15,3,4,7,10);
+        KodillaPatternsApplication.findMinAndMaxSort(3,1,5,6,7,9,15,3,4,7,10);
+
+        System.out.println("Dla wartości 8, 2, 14, 6, 9");
+        KodillaPatternsApplication.findMinAndMaxSimple(8, 2, 14, 6, 9);
+        KodillaPatternsApplication.findMinAndMaxSort(8, 2, 14, 6, 9);
+
+        System.out.println("Dla wartości -3, -7, -1, -20, -4");
+        KodillaPatternsApplication.findMinAndMaxSimple(-3, -7, -1, -20, -4);
+        KodillaPatternsApplication.findMinAndMaxSort(-3, -7, -1, -20, -4);
+
+        System.out.println("Dla wartości -10, 5, 0, 23, -7, 8");
+        KodillaPatternsApplication.findMinAndMaxSimple(-10, 5, 0, 23, -7, 8);
+        KodillaPatternsApplication.findMinAndMaxSort(-10, 5, 0, 23, -7, 8);
+
+        System.out.println("Dla wartości 42");
+        KodillaPatternsApplication.findMinAndMaxSimple(42);
+        KodillaPatternsApplication.findMinAndMaxSort(42);
+
+        System.out.println("Dla wartości 5, 5, 5, 5, 5");
+        KodillaPatternsApplication.findMinAndMaxSimple(5, 5, 5, 5, 5);
+        KodillaPatternsApplication.findMinAndMaxSort(5, 5, 5, 5, 5);
+
+        System.out.println("Dla wartości 1000, -1000, 500, -500, 0");
+        KodillaPatternsApplication.findMinAndMaxSimple(1000, -1000, 500, -500, 0);
+        KodillaPatternsApplication.findMinAndMaxSort(1000, -1000, 500, -500, 0);
+
+        System.out.println("Dla wartości 9, 1, 8, 2, 7, 3, 6, 4, 5");
+        KodillaPatternsApplication.findMinAndMaxSimple(9, 1, 8, 2, 7, 3, 6, 4, 5);
+        KodillaPatternsApplication.findMinAndMaxSort(9, 1, 8, 2, 7, 3, 6, 4, 5);
     }
 
 
@@ -37,5 +71,29 @@ public class KodillaPatternsApplication {
         else {
             return fibonacci(index - 1, next, next.add(current));
         }
+    }
+
+    public static void findMinAndMaxSimple(int... numbers) {
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) min = numbers[i];
+            if (numbers[i] > max) max = numbers[i];
+        }
+
+        System.out.println("MIN: " + min +  ", MAX: " + max);
+    }
+
+    public static void findMinAndMaxSort(int... numbers) {
+        ArrayList<Integer> sorted = new ArrayList<>();
+
+        for (int number : numbers) {
+            sorted.add(number);
+        }
+
+        sorted.sort(Comparator.comparingInt(a -> a));
+
+        System.out.println("MIN: " + sorted.getFirst() +  ", MAX: " + sorted.getLast());
     }
 }
