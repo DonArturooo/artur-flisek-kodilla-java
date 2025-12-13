22451b8f6f847d87e02b8c8e1767357da58f4e06
Artur Flisek <artur.flisek@pentacomp.pl>
2025-12-13 21:57:50 +0100

    12.6 Dodanie materiałów i zadania

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,95 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DbManagerTestSuite {
+
+    @Test
+    void testConnection() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+
+        assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection()
+                                       .createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " + rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(5, counter);
+    }
+
+    @Test
+    void testSelectUsersAndTasks() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = """
+                SELECT U.FIRSTNAME, U.LASTNAME, I.SUMMARY
+                  FROM USERS U
+                  JOIN ISSUES I ON U.ID = I.USER_ID_ASSIGNEDTO
+                """;
+        Statement statement = dbManager.getConnection()
+                                       .createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME") + ", " + rs.getString("SUMMARY"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(15, counter);
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = """
+                SELECT U.FIRSTNAME, U.LASTNAME
+                FROM POSTS P
+                         JOIN USERS U ON P.USER_ID = U.ID
+                GROUP BY U.ID
+                HAVING COUNT(*) > 1;
+                """;
+        Statement statement = dbManager.getConnection()
+                                       .createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(2, counter);
+    }
+}
