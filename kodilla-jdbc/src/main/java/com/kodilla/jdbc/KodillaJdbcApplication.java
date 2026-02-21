22508f9230aa3b0db3179a9b94ee104d82789d93
Artur <artur.flisek@gmail.com>
2026-02-21 17:31:10 +0100

    22.6 Dodanie zadania dodatkowego

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/KodillaJdbcApplication.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/KodillaJdbcApplication.java
@@ -10,4 +10,29 @@ public class KodillaJdbcApplication {
         SpringApplication.run(KodillaJdbcApplication.class, args);
     }
 
+    public static int[][] convertMother(int[][] mother) {
+        int[][] matrix = new int[mother[0].length][mother.length];
+
+        System.out.println("Converting mother " + mother.length + " x " + mother[0].length + " to matrix..." + matrix.length + " x " + matrix[0].length);
+
+        for (int i = 0; i < mother.length; i++) {
+            for(int j = 0; j < mother[0].length; j++) {
+                matrix[j][i] = mother[i][j];
+            }
+        }
+        System.out.println("Result matrix:");
+        printArray(matrix);
+
+        return matrix;
+    }
+
+    private static void printArray(int[][] array) {
+        for (int[] row : array) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/KodillaJdbcApplicationTests.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/KodillaJdbcApplicationTests.java
@@ -3,6 +3,9 @@ package com.kodilla.jdbc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.kodilla.jdbc.KodillaJdbcApplication.convertMother;
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class KodillaJdbcApplicationTests {
 
@@ -10,4 +13,23 @@ class KodillaJdbcApplicationTests {
     void contextLoads() {
     }
 
+
+    @Test
+    void testConvertMother() {
+        //Given
+        int[][] mother = new int[][]{{5,3,-2},{-7,4,8},{2,7,1},{9,5,3}};
+        int[][] result = new int[][]{{5, -7, 2, 9},{3, 4, 7, 5},{-2, 8, 1, 3}};
+
+        //When
+        int[][] matrix = convertMother(mother);
+
+        //Then
+        assertAll(
+                () -> assertEquals(matrix.length, mother[0].length),
+                () -> assertEquals(matrix[0].length, mother.length),
+                () -> assertArrayEquals(matrix[0], result[0], "Wrong matrix"),
+                () -> assertArrayEquals(matrix[1], result[1], "Wrong matrix"),
+                () -> assertArrayEquals(matrix[2], result[2], "Wrong matrix")
+        );
+    }
 }
